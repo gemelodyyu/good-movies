f64e540195c413941d8125d5dc1caa9f30e6cac2
Johnston1215 <johnston1215@yahoo.com>
2020-11-21 11:13:49 -0600

    Saturday class

--- Test_data.js
+++ Test_data.js
@@ -1,89 +1,89 @@
-var data = [
-    {id: 862,
-      title: "Toy Story",
-      release_date: "1995-10-30",
-      runtime: 81,
-      adult: false,
-      budget: 30000000,
-      revenue: 373554033,
-      oscar_nominations: true
-      },
-      {
-      id: 8844,
-      title: "Jumanji",
-      release_date: "1996-12-15",
-      runtime: 104,
-      adult: false,
-      budget: 65000000,
-      revenue: 262797249,
-      oscar_nominations: false
-      },
-      {
-      id: 31357,
-      title: "Waiting to Exhale",
-      release_date: "1997-12-22",
-      runtime: 127,
-      genres: "[{‘id’: 35, ‘name’: ‘Comedy’}, {‘id’: 18, ‘name’: ‘Drama’}, {‘id’: 10749, ‘name’: ‘Romance’}]",
-      adult: false,
-      budget: 16000000,
-      revenue: 81452156,
-      oscar_nominations: true
-      },
-      {
-      id: 11862,
-      title: "Father of the Bride Part II",
-      release_date: "1998-02-10",
-      runtime: 106,
-      genres: "[{‘id’: 35, ‘name’: ‘Comedy’}]",
-      adult: false,
-      budget: 0,
-      revenue: 76578911,
-      production_companies: "[{‘name’: ‘Sandollar Productions’, ‘id’: 5842}, {‘name’: ‘Touchstone Pictures’, ‘id’: 9195}]",
-      imdb_id: "[{‘iso_3166_1’: ‘US’, ‘name’: ‘United States of America’}]",
-      oscar_nominations: false
-      },
-      {
-      id: 862,
-      title: "Toy Story",
-      release_date: "1995-10-30",
-      runtime: 81,
-      adult: false,
-      budget: 30000000,
-      revenue: 373554033,
-      oscar_nominations: true
-      },
-      {
-      id: 8844,
-      title: "Jumanji",
-      release_date: "1996-12-15",
-      runtime: 104,
-      adult: false,
-      budget: 65000000,
-      revenue: 262797249,
-      oscar_nominations: false
-      },
-      {
-      id: 31357,
-      title: "Waiting to Exhale",
-      release_date: "1997-12-22",
-      runtime: 127,
-      genres: "[{‘id’: 35, ‘name’: ‘Comedy’}, {‘id’: 18, ‘name’: ‘Drama’}, {‘id’: 10749, ‘name’: ‘Romance’}]",
-      adult: false,
-      budget: 16000000,
-      revenue: 81452156,
-      oscar_nominations: true
-      },
-      {
-      id: 11862,
-      title: "Father of the Bride Part II",
-      release_date: "1998-02-10",
-      runtime: 106,
-      genres: "[{‘id’: 35, ‘name’: ‘Comedy’}]",
-      adult: false,
-      budget: 0,
-      revenue: 76578911,
-      production_companies: "[{‘name’: ‘Sandollar Productions’, ‘id’: 5842}, {‘name’: ‘Touchstone Pictures’, ‘id’: 9195}]",
-      imdb_id: "[{‘iso_3166_1’: ‘US’, ‘name’: ‘United States of America’}]",
-      oscar_nominations: false
-      }
-  ];
+// var data = [
+//     {id: 862,
+//       title: "Toy Story",
+//       release_date: "1995-10-30",
+//       runtime: 81,
+//       adult: false,
+//       budget: 30000000,
+//       revenue: 373554033,
+//       oscar_nominations: true
+//       },
+//       {
+//       id: 8844,
+//       title: "Jumanji",
+//       release_date: "1996-12-15",
+//       runtime: 104,
+//       adult: false,
+//       budget: 65000000,
+//       revenue: 262797249,
+//       oscar_nominations: false
+//       },
+//       {
+//       id: 31357,
+//       title: "Waiting to Exhale",
+//       release_date: "1997-12-22",
+//       runtime: 127,
+//       genres: "[{‘id’: 35, ‘name’: ‘Comedy’}, {‘id’: 18, ‘name’: ‘Drama’}, {‘id’: 10749, ‘name’: ‘Romance’}]",
+//       adult: false,
+//       budget: 16000000,
+//       revenue: 81452156,
+//       oscar_nominations: true
+//       },
+//       {
+//       id: 11862,
+//       title: "Father of the Bride Part II",
+//       release_date: "1998-02-10",
+//       runtime: 106,
+//       genres: "[{‘id’: 35, ‘name’: ‘Comedy’}]",
+//       adult: false,
+//       budget: 0,
+//       revenue: 76578911,
+//       production_companies: "[{‘name’: ‘Sandollar Productions’, ‘id’: 5842}, {‘name’: ‘Touchstone Pictures’, ‘id’: 9195}]",
+//       imdb_id: "[{‘iso_3166_1’: ‘US’, ‘name’: ‘United States of America’}]",
+//       oscar_nominations: false
+//       },
+//       {
+//       id: 862,
+//       title: "Toy Story",
+//       release_date: "1995-10-30",
+//       runtime: 81,
+//       adult: false,
+//       budget: 30000000,
+//       revenue: 373554033,
+//       oscar_nominations: true
+//       },
+//       {
+//       id: 8844,
+//       title: "Jumanji",
+//       release_date: "1996-12-15",
+//       runtime: 104,
+//       adult: false,
+//       budget: 65000000,
+//       revenue: 262797249,
+//       oscar_nominations: false
+//       },
+//       {
+//       id: 31357,
+//       title: "Waiting to Exhale",
+//       release_date: "1997-12-22",
+//       runtime: 127,
+//       genres: "[{‘id’: 35, ‘name’: ‘Comedy’}, {‘id’: 18, ‘name’: ‘Drama’}, {‘id’: 10749, ‘name’: ‘Romance’}]",
+//       adult: false,
+//       budget: 16000000,
+//       revenue: 81452156,
+//       oscar_nominations: true
+//       },
+//       {
+//       id: 11862,
+//       title: "Father of the Bride Part II",
+//       release_date: "1998-02-10",
+//       runtime: 106,
+//       genres: "[{‘id’: 35, ‘name’: ‘Comedy’}]",
+//       adult: false,
+//       budget: 0,
+//       revenue: 76578911,
+//       production_companies: "[{‘name’: ‘Sandollar Productions’, ‘id’: 5842}, {‘name’: ‘Touchstone Pictures’, ‘id’: 9195}]",
+//       imdb_id: "[{‘iso_3166_1’: ‘US’, ‘name’: ‘United States of America’}]",
+//       oscar_nominations: false
+//       }
+//   ];

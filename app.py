9aec1bd6ea1dab8ee981e0bfb58d23250b4221a0
gemelodyyu <yuge.chang0512@gmail.com>
2020-11-21 14:04:05 -0600

    Nov. 21 update by M

--- app.py
+++ app.py
@@ -28,11 +28,11 @@ def index():
 def star():
     return render_template("star.html")
 
-@app.route('/data')
+@app.route('/data.html')
 def data():
     return render_template("data.html")
 
-@app.route('/api/data.html')
+@app.route('/api/data')
 def fetchData():
     cursor.execute('SELECT m.title, m.genres, m.release_date, c.movie_crew, c.movie_cast, m.overview, r.rating FROM movie_meta m INNER JOIN credits c ON m.id = c.id INNER JOIN ratings r ON m.id = r."movieId" LIMIT 10')
     columns = ('title', 'genres', 'release_date', 'movie_crew', 'movie_cast', 'overview', 'rating')
